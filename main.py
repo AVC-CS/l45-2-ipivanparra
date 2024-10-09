e019120ad0f24adebe5854996ae15e88f13110e9
Ivan <parraivan127@gmail.com>
2024-10-09 15:56:26 -0700

    L-4 (5-2)

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     total = 0
     i = 0
     for i in range(5):                              # Range of 5 numbers. 
-        numbers.apped(random.randint(0,100))        # Use the append function to add the numbers you obtain to the list.
+        numbers.append(random.randint(0,100))        # Use the append function to add the numbers you obtain to the list.
         print (numbers[i], end = ' ')               # print numbers with spacing. 
         total += numbers[i]                         # New total is from the previous total plus the new number. 
     print(total, end='')
