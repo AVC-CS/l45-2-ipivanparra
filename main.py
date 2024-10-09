c73f423166b22894b97a92647270c4b0ed5bc8ca
Ivan <parraivan127@gmail.com>
2024-10-09 15:47:28 -0700

    Commit 1

--- main.py
+++ main.py
@@ -4,11 +4,9 @@ import random
 def main():
     total = 0
     numbers = []
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    total = 0
+    i = 0
+    
 
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
