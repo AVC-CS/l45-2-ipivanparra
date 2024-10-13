1e0ab29e5745203e2f3c05486a97c9e55744caa5
Ivan <parraivan127@gmail.com>
2024-10-12 22:24:19 -0700

    L-4 (5-2) ver.6

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def main():
     for i in range(5):                           
         numbers.append(random.randint(0,100)) 
         print (numbers[i], end= ' ')
-        if total > 100:
+        if total > 100:                     # if-else statement to state that if the total is greater than 100 break. 
                 break
-        else: total += numbers[i]
+        else: total += numbers[i]           # else continue adding to the total. 
 
     print(total)
             
